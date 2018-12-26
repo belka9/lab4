f6a2d5592c2fda8123527820be79ca866d8da0e5
belka9 <43209776+belka9@users.noreply.github.com>
2018-12-26 14:18:20 +0700

    main.c
    with change

--- main.c
+++ main.c
@@ -1,62 +1,64 @@
 #include <stdio.h>
 #include <string.h>
-#define swap(x,y) x ^= y;y ^= x;x ^= y;
+#define SIZE_NUMBER 10
 
-int check(char str[10], int len) {
-    for (int i = 0; i < len; i++) {
-        if (str[i] > '9' || str[i] < '0')
-            return 1;
-    }
-    int g = 1;
-    for (int i = len - 2; i >= 0; --i) {
-        if (str[i] == str[i + 1])
-            ++g;
-    }
-    if (g == len)
-        return 1;
-    return 0;
+void swap(char *a, char *b){
+    char t = *a;
+    *a = *b;
+    *b = t;
 }
 
-int dijktraPerm(char str[10], int len) {
-    int i = len - 2;
-    while (i != -1 && str[i] >= str[i + 1]) {
-        --i;
-        if (i == -1)
-            return 1;//перестановок больше нет
+int check(const char str[], int len) {
+    int arr[SIZE_NUMBER] = {0};
+    for (int i = 0; i < len; i++){
+        if (str[i] > '9' || str[i] < '0')
+            return 1;
+        if (++arr[str[i] - '0'] > 1)
+            return 1;
     }
-    int j = len - 1;
-
-    while (str[i] >= str[j])
-        --j;
-    swap(str[i], str[j]);
-    int pos = i + 1;//с этого момента пошла сортировка
-    int end = len - 1;
-    while (pos < end) {
-        swap(str[pos], str[end]);
-        ++pos;
-        --end;
-    }
-    printf("%s\n", str);
-
     return 0;
+
+}
+
+void dijktraPerm(char str[], int len, int n) {
+    int count = 0;
+    while (count++ < n) {
+        int i = len - 2;
+        while (i != -1 && str[i] >= str[i + 1]) {
+            --i;
+            if (i == -1)
+                return;//РїРµСЂРµСЃС‚Р°РЅРѕРІРѕРє Р±РѕР»СЊС€Рµ РЅРµС‚
+        }
+        int j = len - 1;
+
+        while (str[i] >= str[j])
+            --j;
+        swap(&str[i], &str[j]);
+        int pos = i + 1;//СЃ СЌС‚РѕРіРѕ РјРѕРјРµРЅС‚Р° РїРѕС€Р»Р° СЃРѕСЂС‚РёСЂРѕРІРєР°
+        int end = len - 1;
+        while (pos < end) {
+            swap(&str[pos], &str[end]);
+            ++pos;
+            --end;
+        }
+        printf("%s\n", str);
+    }
 }
 
 int main() {
-    char a[10];
-    int n, len;
+    char a[SIZE_NUMBER] = {0};
+    int n = 0, len = 0;
     scanf("%s", a);
     len = strlen(a);
     scanf("%d", &n);
-    if (len == 1)
+    if (len == 1 || n <= 0)
         return 0;
 
     if (check(a, len) == 1) {
         printf("bad input");
         return 0;
     }
-
-    while (n != 0 && dijktraPerm(a, len) == 0)
-        --n;
-
+    dijktraPerm(a, len, n);
     return 0;
+
 }
